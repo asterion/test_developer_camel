74d583a66301f8d60ea5ebc25006b3def65376e4
Marcos Matamala <mmatamala1982@gmail.com>
2020-08-24 08:26:36 -0400

    [add] avance en listar usuarios desde api

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      users: [],
+      message: 'Hola'
+    }
+  },
+  mounted() {
+    axios
+      .get('/api/users')
+      .then(response => (this.users = response))
+  }
+})
